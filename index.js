c38c367ef49b01c35dfeed63af514a9b90db510a
Joshua Laurence <laurence11@fb.com>
2023-01-11 11:19:34 +0000

    Added the initial get endpoint, plus added functionaltiy for searching by both content and tags, and the json returned is formatted

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
+const {Joke, sequelize} = require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/jokes', async (req, res, next) => {
-  try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
-  } catch (error) {
-    console.error(error);
-    next(error)
-  }
+app.get("/jokes", async (req, res, next) => {
+	try {
+		let {content, tags} = req.query;
+		let jokes = [];
+
+		if (!content) content = "";
+		if (!tags) tags = "";
+
+		console.log("tags", tags, "- content", content);
+
+		if (content === "" && tags === "") {
+			jokes = await Joke.findAll({});
+		} else {
+			jokes = await Joke.findAll({
+				where: {
+					content: sequelize.where(
+						sequelize.fn("LOWER", sequelize.col("joke")),
+						"LIKE",
+						"%" + content.toLowerCase() + "%"
+					),
+					tags: sequelize.where(
+						sequelize.fn("LOWER", sequelize.col("tags")),
+						"LIKE",
+						"%" + tags.toLowerCase() + "%"
+					),
+				},
+			});
+		}
+		res.type("json").send(JSON.stringify(jokes, null, 2) + "\n");
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
 });
 
 // we export the app, not listening in here, so that we can run tests

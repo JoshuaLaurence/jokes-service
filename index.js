a00ad5c8e4ce54c146f0d3b2221307caf9cf0351
Joshua Laurence <laurence11@fb.com>
2023-01-13 10:19:50 +0000

    Completed the extension tasks; delete, put and create endpoints for the jokes-service

--- index.js
+++ index.js
@@ -25,15 +25,108 @@ app.get("/jokes", async (req, res, next) => {
 						"LIKE",
 						"%" + content.toLowerCase() + "%"
 					),
-					tags: sequelize.where(
-						sequelize.fn("LOWER", sequelize.col("tags")),
-						"LIKE",
-						"%" + tags.toLowerCase() + "%"
-					),
 				},
 			});
+			if (tags) {
+				jokes = jokes.filter((element) => {
+					const element_tags = element.tags.includes(",")
+						? element.tags.split(",").map((element) => element.trim())
+						: [element.tags];
+					tags = Array.isArray(tags) ? tags : [tags];
+
+					console.log(element_tags, tags);
+					if (tags.every((element) => element_tags.includes(element))) return true;
+					return false;
+				});
+			}
 		}
 		res.type("json").send(JSON.stringify(jokes, null, 2) + "\n");
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
+
+app.post("/jokes", async (req, res, next) => {
+	try {
+		const body = req.body;
+		console.log(body);
+		if (!Object.keys(body).includes("content"))
+			throw new Error("Content must be present to create a joke");
+		if (!Object.keys(body).includes("tags"))
+			throw new Error("Tags must be present to create a joke");
+
+		const newJoke = await Joke.create({
+			joke: body.content,
+			tags: body.tags,
+		});
+
+		res
+			.type("json")
+			.status(200)
+			.send(
+				JSON.stringify({message: "New Joke Created", newJoke: newJoke}, null, 2)
+			);
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
+
+app.delete("/jokes/:id", async (req, res, next) => {
+	try {
+		await Joke.destroy({where: {id: req.params.id}});
+		res
+			.type("json")
+			.status(200)
+			.send(
+				JSON.stringify(
+					{message: `Joke of ID ${req.params.id} successfully deleted`},
+					null,
+					2
+				)
+			);
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+});
+
+app.put("/jokes/:id", async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const body = req.body;
+		console.log(body);
+
+		let filteredBody = {};
+		//Removing any keys that aren't correct from the body incase the body were to have more info (metadata) within
+		for (const key in body) {
+			if (key === "tags") {
+				filteredBody[key] = body[key];
+			} else if (key === "content") {
+				filteredBody["joke"] = body[key];
+			}
+		}
+		//presuming the body has already been formatted by the front end correctly
+		//I.E the tags are a single string with no spaces seperated by commas, you can
+		//input the whole body into the update parameter, that way you don't have to check\
+		//if all or both of the keys are present. Whatever is there is directly inputted
+
+		await Joke.update(filteredBody, {where: {id: id}});
+		const updatedResult = await Joke.findByPk(id);
+		res
+			.type("json")
+			.status(200)
+			.send(
+				JSON.stringify(
+					{
+						message: `Joke of ID ${req.params.id} successfully updated`,
+						updatedJoke: updatedResult,
+					},
+					null,
+					2
+				)
+			);
 	} catch (error) {
 		console.error(error);
 		next(error);
